edbe7b5dd352e70da88a62f330921b45663c4ca5
Hiram Todd <htillustrator@gmail.com>
2019-03-21 00:23:10 -0700

    Added Classes 03/21/2019
    Added Student, Enrollment, Course, and data files.

---
+++ ContosoUniversity_HiramTodd/ContosoUniversity_HiramTodd/Models/Enrollment.cs
@@ -0,0 +1,30 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        // The EnrollmentID property will be the primary key; this entity uses 
+        // the classnameID pattern instead of ID by itself as you saw in the 
+        // Student entity
+        public int EnrollmentID { get; set; }
+
+        // The CourseID property is a foreign key, and the corresponding 
+        // navigation property is Course. An Enrollment entity is associated 
+        // with one Course entity
+        public int CourseID { get; set; }
+
+        // The StudentID property is a foreign key, and the corresponding 
+        // navigation property is Student
+        public int StudentID { get; set; } 
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
+
+// This Enrollment class is a navigation property wich hold other entities that are related to this entity
---
+++ ContosoUniversity_HiramTodd/ContosoUniversity_HiramTodd/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        // ID is Primary Key for the database table that corresponds to this class
+        public int ID { get; set; } 
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
