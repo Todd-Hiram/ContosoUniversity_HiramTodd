66e045efe0c381ebbb244e9620f87c36a236126b
Hiram Todd <htillustrator@gmail.com>
2019-03-27 12:58:26 -0700

    Revisions 03/27/2019

--- ContosoUniversity_HiramTodd/ContosoUniversity_HiramTodd/Data/SchoolContext.cs
+++ ContosoUniversity_HiramTodd/ContosoUniversity_HiramTodd/Data/SchoolContext.cs
@@ -13,11 +13,11 @@ namespace ContosoUniversity_HiramTodd.Data
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Student> Students { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Course>().ToTable("Course");
-            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
-            modelBuilder.Entity<Student>().ToTable("Student");
-        }
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<Course>().ToTable("Course");
+        //     modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        //     modelBuilder.Entity<Student>().ToTable("Student");
+        // }
     }
 }
